a5c9293269507dccfddc9817651f90dfe765f8f3
sp3949 <sp3949@columbia.edu>
2021-03-20 11:41:25 -0400

    Fix utils implementation skeleton

--- utils.cpp
+++ utils.cpp
@@ -5,34 +5,30 @@
 
 namespace Proj
 {
-    namespace Utils
+
+    VectorXd Utils::rmse(const double y, const VectorXd &y_hat)
     {
+        //TODO
+        cerr << "Not implemented yet" << endl;
+        const int k = y_hat.size();
 
-        VectorXd rmse(const double y, const VectorXd &y_hat)
-        {
-            //TODO
-            cerr << "Not implemented yet" << endl;
-            const int k = y_hat.size();
+        return VectorXd(k);
+    }
 
-            return VectorXd(k);
-        }
+    double Utils::r2(const double y, const VectorXd &y_hat)
+    {
+        //TODO
+        cerr << "Not implemented yet" << endl;
+        return 0;
+    }
 
-        double r2(const double y, const VectorXd &y_hat)
-        {
-            //TODO
-            cerr << "Not implemented yet" << endl;
-            return 0;
-        }
-
-        pair<double, double> topN(const PMF &pmfModel, const MatrixXd &data,
-                                  int N)
-        {
-            //TODO
-            cerr << "Not implemented yet" << endl;
-            pair<double, double> p;
-            return p;
-        }
-
-    } // namespace Utils
+    pair<double, double> Utils::topN(const PMF &pmfModel, const MatrixXd &data,
+                                     int N)
+    {
+        //TODO
+        cerr << "Not implemented yet" << endl;
+        pair<double, double> p;
+        return p;
+    }
 
 } // namespace Proj
