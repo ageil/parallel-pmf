f44f80db5cfbaf577b4833efcbbd7052e38333de
sp3949 <sp3949@columbia.edu>
2021-03-21 16:59:07 -0400

    Fix rmse impl

--- models/utils.cpp
+++ models/utils.cpp
@@ -1,30 +1,38 @@
 #include "utils.h"
 #include "PMF.h"
 
+#include <cmath>
 #include <iostream>
+
 #include <gsl/gsl_assert>
 
 namespace Model
 {
 
-    VectorXd Utils::rmse(const double y_hat, const VectorXd &y)
+    double Utils::rmse(const VectorXd &y, const double y_hat)
     {
-        const double denominator = y.size();
+        const auto denominator = y.size();
         Expects(denominator > 0);
 
-        const double c = 1 / denominator;
+        const double c = 1 / (double)denominator;
+        const VectorXd &squared = (y.array() - y_hat).square();
+        const double &summed = squared.sum();
 
-        // sqrt(1/k * sum((y-y_hat^2)))
-        VectorXd sub = (c * (y_hat - y.array()).square()).sqrt();
-        return sub;
+        return sqrt(c * summed);
     }
 
-    double Utils::r2(const double y_hat, const VectorXd &y)
+    double Utils::r2(const VectorXd &y, const VectorXd &y_hat)
     {
         //TODO
         cerr << "Not implemented yet" << endl;
         return 0;
     }
+
+    // def R2(y, y_hat):
+    // SSE = np.sum((y - y_hat)**2)
+    // TSS = np.sum((y - np.mean(y))**2)
+    // R2 = 1 - SSE/TSS
+    // return R2
 
     pair<double, double> Utils::topN(const PMF &pmfModel, const MatrixXd &data,
                                      const int N)
